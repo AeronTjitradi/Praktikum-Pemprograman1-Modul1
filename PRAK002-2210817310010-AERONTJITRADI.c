b091d75f0d254558deb7dc0662b7a0e3151c7c44
AeronTjitradi <115365108+AeronTjitradi@users.noreply.github.com>
2022-10-09 00:27:36 +0800

    C

---
+++ PRAK002-2210817310010-AERONTJITRADI.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (){
+    printf("Selamat Pagi, Aeron Tjitradi\n");
+    printf("Selamat Siang, Aeron Tjitradi\n");
+    printf("Selamat Malam, Aeron Tjitradi");
+
+return 0;
+}
---
+++ PRAK004-2210817310010-AERONTJITRADI.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+    printf("##################################\n");
+    printf("#                                #\n");
+    printf("#         Aeron Tjitradi         #\n");
+    printf("#         2210817310010          #\n");
+    printf("#                                #\n");
+    printf("##################################");
+
+return 0;
+}
